4c9bbbfbc2b76ec5db6b5e8cd38edb63e7841d2a
Elliot Dickerson <elliot@toille.uk>
2025-01-08 23:18:36 +0000

    More consistent 404 page

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,19 +1,24 @@
 import { Title } from "@solidjs/meta";
+import { Paragraph } from "../components/Paragraph";
 import { HttpStatusCode } from "@solidjs/start";
 
 export default function NotFound() {
+  const headingClasses: any = {
+    title: true,
+  };
+
   return (
     <main>
       <Title>Not Found</Title>
       <HttpStatusCode code={404} />
-      <h1>Page Not Found</h1>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <header>
+        <h2
+          classList={headingClasses}
+        >
+          Not Found
+        </h2>
+      </header>
+      <Paragraph paragraph={"I think you're lost!"} last={true} />
     </main>
   );
 }
